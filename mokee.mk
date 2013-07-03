266dd7422e39835fce1c9b446ff9a598be6d42ab
martincz <martincz@mokeetek.com>
2013-07-03 14:39:00 +0800

    merge fix
    Change-Id: If278fcfdfb4505758fe4e2969ddc998d6795e150

--- mokee.mk
+++ mokee.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := Nexus7-GSM
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
-# Inherit some common CM stuff.
+# Inherit some common MK stuff.
 $(call inherit-product, vendor/mk/config/common_full_tablet_wifionly.mk)
 
 # Inherit GSM common stuff
